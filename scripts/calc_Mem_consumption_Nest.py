c88cf846cb973fb385f8cad09f475254ddf6a320
Till Schumann <Till.Schumann@rwth-aachen.de>
2015-11-10 17:46:17 +0100

    und wieder ein neues update

--- scripts/calc_Mem_consumption_Nest.py
+++ scripts/calc_Mem_consumption_Nest.py
@@ -1,24 +1,30 @@
 import numpy as np
 from pylab import *
 
+def PI_N(M,N):
+  Nm = N/M
 
-def PI_NEST(M,T,N,K):
+  return Nm*(24+1100)
+
+def PI_C(M,T,N,K):
   beta=0.8
   Nm = N/M
   Km = Nm*K
-  Kstdp_m = Km * beta**2
-  Kstat_m = Km - Kstdp_m
   Nvp = Nm/T
   Nc1 = (1.-(1./(M*T*Nvp)))**(K/(M*T)-1.)*K/(M*T)
   Kvp = Nvp*K
   Ncb1 = (1.-(1.-1./N)**Kvp-(1.-1./N)**(Kvp-1)*(1/N)*Kvp)*N
-  return 0.33 * T * N + T * Nc1 * 24. + T * Ncb1 * 128. + Kstat_m * 16. + Kstdp_m * 24.
+  return 0.33 * T * N + T * Nc1 * 24. + T * Ncb1 * 128. + Km * 52.
+
+def PI_NEST(M,T,N,K):
+  return PI_N(M,N) + PI_C(M,T,N,K)
 
 
 T = 8.
 #K = 1500.
-K=2500.
-N = 8.0e7
+#K=2500.
+K=11000
+N = 75e6
 
 M = range(1,4096)
 
@@ -39,26 +45,26 @@ show()
 
 
 ###
-L0 = 80./1073741824.
-Ls = 4./1073741824.
-theta = 0.5
+#L0 = 80./1073741824.
+#Ls = 4./1073741824.
+#theta = 0.5
 
 
-WS = [((16.-pi)-9.*L0-4.*Ls*m)/(Ls*(4.+1./theta)) for m, pi in zip(M,PI)]
+#WS = [((16.-pi)-9.*L0-4.*Ls*m)/(Ls*(4.+1./theta)) for m, pi in zip(M,PI)]
 
-C = [N*K/ws for ws in WS]
+#C = [N*K/ws for ws in WS]
 
-plot(M[21:],WS[21:])
-title("predicted maximum chunk size w*s")
-xlabel("number of MPI nodes")
-ylabel("chunk size w*s")
+#plot(M[21:],WS[21:])
+#title("predicted maximum chunk size w*s")
+#xlabel("number of MPI nodes")
+#ylabel("chunk size w*s")
 
 
-show()
+#show()
 
-plot(M[21:],C[21:])
-title("predicted best-case algorithms iterations with maximum chunk size")
-xlabel("number of MPI nodes")
-ylabel("number of iterations")
+#plot(M[21:],C[21:])
+#title("predicted best-case algorithms iterations with maximum chunk size")
+#xlabel("number of MPI nodes")
+#ylabel("number of iterations")
 
-show()
+#show()
